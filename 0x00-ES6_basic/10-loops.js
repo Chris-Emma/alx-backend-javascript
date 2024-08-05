06efde84f3a88798d44a8a05fa69e6342f1c38ed
Chris-Emma <chrisemmaofficial@gmail.com>
2024-08-05 17:02:15 +0000

    es6-basics

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const updatedArray = [];
+
+  for (const idx of array) {
+    const value = idx;
+    updatedArray.push(appendString + value);
+  }
+
+  return updatedArray;
+}
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,1 @@
+This file contains  the tasks for the ES6 Basics project, which is part of the ALXSE short specializations curriculum focused on modern JavaScript, particularly ECMAScript 6 (ES6).
