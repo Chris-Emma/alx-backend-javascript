213ab8b35b9a9a04eab359e9147117eb16b610ff
Chris-Emma <chrisemmaofficial@gmail.com>
2024-08-09 17:04:46 +0000

    promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function getResponseFromAPI() {
+  return new Promise(() => {
+    // API call or asynchronous operation
+    // Resolve or reject the promise
+  });
+}
+
+export default getResponseFromAPI;
